174dd63176b0c8cdfd220113663d3a4daee0fced
edwin_anajemba <anajembaedwin@gmail.com>
2022-10-07 13:01:55 +0000

    Updated promptUser( ) and added reAttempt( )

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
-console.log("Welcome! Would you like to play a Guessing Game?")
-console.log("------------------------------------------------")
+let user = prompt(`What is your name?`)
+console.clear()
+let greeting = `Welcome ${user}! Would you like to play a Guessing Game?`
+let underLine = "-"
+
+// console.log(greeting.length)
+underLine = underLine.repeat(greeting.length)
+console.log(`${greeting}`)
+console.log(`${underLine}`)
 console.log()
+
+
 
 let Y = "Yes"
 let N = "No"
 let accept = prompt(`Type ${Y} or ${N} `)
+let guessRange = []
+let pointsGotten = "0"
 
-//condition to check the value of Y and N
-if (accept == Y){
-  console.log()
-  let guess = prompt("Enter a guess between 1 and 2 ")
-  console.log()
-  console.log(`Your guess is ${guess}`)
-} else {
-  console.log()
-  console.log("We hate to see you leave, come back and play again.")
+
+
+//function to check the value of Y and N
+function promptUser() {
+    if (accept == Y){
+    console.log()
+    let guess = prompt("Enter a guess between 1 and 2 ")
+    console.log()
+      if (guess >=1 && guess <=2){
+        console.log(`Your guess is ${guess}`)
+        console.log()
+        guessRange.push(guess)
+        console.log(guessRange)
+      } else {
+        console.clear()
+        reAttempt()
+      }
+    
+  } else {
+    console.log()
+    console.log("We hate to see you leave, come back and play again.")
+  }
 }
 
-let guessRange = []
-let pointsGotten = []
+promptUser()
+
+//function to run a re-attempt when guess fails
+function reAttempt() {
+  console.log()
+  console.log(`Your guess is invalid, try again please`)
+  console.log()
+  promptUser()
+}
+
+//function to add and update points gotten by player
+function addPoints() {
+  pointsInNumber = parseInt(pointsGotten, 10)
+  // console.log(typeof(pointsInNumber))
+  // console.log(pointsInNumber)
+}
+
+addPoints()
 
 
